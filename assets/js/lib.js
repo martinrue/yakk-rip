726fac91449c636ff147d00f78dbd00260b8d98d
Martin Rue <hello@martinrue.com>
2025-07-31 17:36:47 +0700

    First commit

---
+++ assets/js/lib.js
@@ -0,0 +1,10 @@
+const lib = (() => {
+  const onReady = (fn) => {
+    document.addEventListener("DOMContentLoaded", fn);
+  };
+
+  const $ = (selector) => document.querySelector(selector);
+  const $$ = (selector) => document.querySelectorAll(selector);
+
+  return { onReady, $, $$ };
+})();
